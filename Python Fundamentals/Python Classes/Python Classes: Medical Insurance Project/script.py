a071b81ad4174fd40e3c2a3c89e98c67981b128b
peter-j-schmidt <schmidt.peter.john@gmail.com>
2020-11-10 21:15:18 -0500

    Added code for python classes project.

---
+++ Python Fundamentals/Python Classes/Python Classes: Medical Insurance Project/script.py
@@ -0,0 +1,52 @@
+class Patient:
+      def __init__(self, name, age, sex, bmi, num_of_children, smoker):
+    self.name = name
+    self.age = age
+    # add more parameters here
+    self.sex = sex
+    self.bmi = bmi
+    self.num_of_children = num_of_children
+    self.smoker = smoker
+
+  def estimated_insurance_cost(self):
+    estimated_cost = 250 * self.age - 128 * self.sex + 370 * self.bmi + 425 * self.num_of_children + 24000 * self.smoker - 12500
+    print(self.name + "'s estimated insurance cost is " + str(estimated_cost) + " dollars.")
+
+  def update_age(self, new_age):
+    self.age = new_age
+    print(self.name + " is now " + str(self.age) + " years old.")
+
+  def update_num_children(self, new_num_children):
+    self.num_of_children = new_num_children
+    if self.num_of_children == 1:
+      print(self.name + " has " + str(self.num_of_children) + " child.")
+    else:
+      print(self.name + " has " + str(self.num_of_children) + " children.")
+
+  def patient_profile(self):
+    patient_information = {}
+    patient_information["Name"] = self.name
+    patient_information["Age"] = self.age
+    patient_information["Sex"] = self.sex
+    patient_information["BMI"] = self.bmi
+    patient_information["Number of Children"] = self.num_of_children
+    patient_information["Smoker"] = self.smoker
+    return patient_information
+  
+
+patient1 = Patient("John Doe", 25, 1, 22.2, 0, 0)
+
+# testing the __init__ method
+print(patient1.name)
+patient1.estimated_insurance_cost()
+
+# testing update_age method
+patient1.update_age(26)
+patient1.estimated_insurance_cost()
+
+# testing update_num_children method
+patient1.update_num_children(1)
+patient1.estimated_insurance_cost()
+
+# testing patient_profile method
+print(patient1.patient_profile())
